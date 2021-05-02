30c21b710ca8fee8cd36da6eb1f4bbe52dc7723b
Malhar <mnerurka@ucsd.edu>
2021-05-01 21:14:57 -0700

    Finished parts 1-3

--- script.js
+++ script.js
@@ -1,17 +1,68 @@
 // script.js
 
 const img = new Image(); // used to load image from <input> and draw to canvas
+const canvas = document.getElementById("user-image"); //load the canvas 
+const ctx = canvas.getContext("2d"); //create the context object.
+const input = document.getElementById("image-input"); //load image input file
+const form = document.getElementById("generate-meme"); //load form
+
 
 // Fires whenever the img object loads a new image (such as with img.src =)
 img.addEventListener('load', () => {
-  // TODO
+  ctx.clearRect(0, 0, canvas.width, canvas.height); //clear the canvas
 
-  // Some helpful tips:
-  // - Fill the whole Canvas with black first to add borders on non-square images, then draw on top
-  // - Clear the form when a new image is selected
-  // - If you draw the image to canvas here, it will update as soon as a new image is selected
+  var dim = getDimensions(canvas.width, canvas.height, img.width, img.height); //calculate dimensions
+
+  ctx.fillStyle = "black";  
+  ctx.fillRect(0, 0, canvas.width, canvas.height);  //fill the entire canvas with black
+  
+  ctx.drawImage(img, dim.startX, dim.startY, dim.width, dim.height);  //draw the image with the correct dimensions onto the canvas
+
 });
 
+//fires when image file is selected
+input.addEventListener('change', loadImage);
+
+function loadImage(event) { //function to update src and alt of image
+  var input_file = event.target.files[0]; //select the correct input file
+
+  if(input_file != null){
+    img.src = URL.createObjectURL(input_file); //create URL to image file
+    img.alt = input_file.name;  //set img alt to the file name
+
+    URL.revokeObjectURL(input_file); //free memory allocated by URL object
+  }
+}
+
+//fires when user clicks 'generate'
+form.addEventListener('submit', drawText);
+
+function drawText(event){ //function to draw text to the screen
+
+  //select the text elements
+  var toptext = document.getElementById("text-top").value;
+  var bottomtext = document.getElementById("text-bottom").value;
+  //select the button elements
+  var clearbtn = document.getElementById("button-group").querySelector('button[type="reset"]');
+  var readtxtbtn = document.getElementById("button-group").querySelector('button[type="button"]');
+  var submitbtn = document.getElementById("generate-meme").querySelector('button[type="submit"]');
+
+  //enable clear and read text buttons
+  clearbtn.disabled=false;
+  readtxtbtn.disabled=false;
+  //disable the generate button
+  submitbtn.disabled=true;
+
+  //draw top and bottom text on the canvas
+  ctx.font = "40px Comic Sans MS";
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+  ctx.fillText(toptext, canvas.width/2, 50);
+  ctx.fillText(bottomtext, canvas.width/2, 370);
+
+  //prevent the page from refreshing after form is submitted
+  event.preventDefault();
+}
 /**
  * Takes in the dimensions of the canvas and the new image, then calculates the new
  * dimensions of the image so that it fits perfectly into the Canvas and maintains aspect ratio
@@ -23,7 +74,7 @@ img.addEventListener('load', () => {
  * and also the starting X and starting Y coordinate to be used when you draw the new image to the
  * Canvas. These coordinates align with the top left of the image.
  */
-function getDimmensions(canvasWidth, canvasHeight, imageWidth, imageHeight) {
+function getDimensions(canvasWidth, canvasHeight, imageWidth, imageHeight) {
   let aspectRatio, height, width, startX, startY;
 
   // Get the aspect ratio, used so the picture always fits inside the canvas
